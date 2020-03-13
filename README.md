8bc9d0a537abf464355f141f8b7445e12b768935
thcl_hkr <kayshcache@github.com>
2020-03-13 19:55:19 +1100

    ...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cpp-lab-wk2
+g++ (GCC) 9.2.1 20190827 (Red Hat 9.2.1-1)
+Copyright (C) 2019 Free Software Foundation, Inc.
+This is free software; see the source for copying conditions.  There is NO
+warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
--- build_table.cpp
+++ build_table.cpp
@@ -3,16 +3,20 @@
 int setChairs();
 int setSurfaceArea();
 std::string setColor();
-char setWoodType(); // m mahogony, o oak, p pine // reject other with conditional
-void calculatePrice();
-void displayDetails(int chairs, int surface_area, std::string color); // table dimensions, wood, chair numbers
-void displayPrice();
+std::string setWoodType(); // m mahogony, o oak, p pine // reject other with conditional
+int calculatePrice(int chairs, int surface_area, std::string color, std::string wood);
+void displayDetails(int chairs, int surface_area, std::string color, std::string wood); // table dimensions, wood, chair numbers
+void displayPrice(int price);
 
 int main() {
 	int chairs = setChairs();
 	int surface_area = setSurfaceArea();
 	std::string color = setColor();
-	displayDetails(chairs, surface_area, color);
+	std::string wood = setWoodType();
+	displayDetails(chairs, surface_area, color, wood);
+	int price = calculatePrice(chairs, surface_area, color, wood);
+	displayPrice(price);
+	return 0;
 }
 
 int setChairs() {
@@ -36,13 +40,49 @@ std::string setColor() {
 	return color;
 }
 
-char setWoodType() {
-	char wood_type;
-	std::cin << 
+std::string setWoodType() {
+	while (true) {
+		char wood_type;
+		std::cout << "Choose your wood, enter: m, o, or p" << std::endl;
+		std::cin >> wood_type;
+		const char mahogany = 'm';
+		const char oak = 'o';
+		const char pine = 'p';
+		if (wood_type == mahogany) {
+			return "mahogany";
+		} else if (wood_type == oak) {
+			return "oak";
+		} else if (wood_type == pine) {
+			return "pine";
+		} else {
+			std::cout << "Try again" << std::endl;
+			continue;
+		}
+	}
 }
 
-void displayDetails(int chairs, int surface_area, std::string color) {
+int calculatePrice(int chairs, int surface_area, std::string color, std::string wood) {
+	int wood_price;
+	if (wood == "mahogany") {
+		wood_price = 200;
+	} 
+	if (wood == "pine") {
+		wood_price = 95;
+	} 
+	if (wood == "oak") {
+		wood_price = 150;
+	}
+	double table_price = wood_price * (surface_area + (chairs / 2));
+	return table_price;
+}
+
+void displayDetails(int chairs, int surface_area, std::string color, std::string wood) {
 	std::cout << "Number of chairs is: " << chairs << std::endl;
 	std::cout << "Surface area is: " << surface_area << std::endl;
 	std::cout << "Color is: " + color << std::endl;
+	std::cout << "Wood type is: " + wood << std::endl;
+}
+
+void displayPrice(int price) {
+	std::cout << "The price will be: $" << price << std::endl;
 }
